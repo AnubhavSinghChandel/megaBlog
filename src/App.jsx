8b4b4fea7eeaea0396221e4fac2fd07c303ec9ee
Anubhav Singh Chandel <anubhavsinghchandel1498@gmail.com>
2023-11-30 19:20:08 +0530

    added auth serive functions for appwrite

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="w-full h-full flex flex-col align-middle bg-black p-4">
+        <h1 className="bg-emerald-700 text-white p-2">Hello</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
